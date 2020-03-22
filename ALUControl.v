cd80de9ea53c9c3050a1151f616849288dbeffb6
alfonso46674 <49523948+alfonso46674@users.noreply.github.com>
2020-03-21 23:30:29 -0600

    Operaciones Básicas
    Se añaden las siguientes operaciones:
    Tipo R:
    ADD, SUB, OR, NOR, AND
    Tipo I:
    ORI,ADDI

--- ALUControl.v
+++ ALUControl.v
@@ -24,6 +24,8 @@ localparam R_Type_AND    = 9'b111_100100;
 localparam R_Type_OR     = 9'b111_100101;
 localparam R_Type_NOR    = 9'b111_100111;
 localparam R_Type_ADD    = 9'b111_100000;
+localparam R_Type_SUB	 = 9'b111_100010;
+
 localparam I_Type_ADDI   = 9'b100_xxxxxx;
 localparam I_Type_ORI    = 9'b101_xxxxxx;
 
@@ -36,6 +38,12 @@ always@(Selector)begin
 	casex(Selector)
 		R_Type_AND:    ALUControlValues = 4'b0000;
 		R_Type_OR: 		ALUControlValues = 4'b0001;
+		R_Type_NOR:		ALUControlValues = 4'b0010;
+		R_Type_ADD:		ALUControlValues = 4'b0011;
+		R_Type_SUB:		ALUControlValues = 4'b0100;
+		
+		I_Type_ADDI:	ALUControlValues = 4'b0011;
+		I_Type_ORI:		ALUControlValues = 4'b0001;
 
 		
 		default: ALUControlValues = 4'b1001;
